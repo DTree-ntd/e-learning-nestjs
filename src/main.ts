a419e0792c2fca546a140926a42826c32f46099e
duc nguyen <ngtrungduc29@gmail.com>
2023-03-01 13:42:47 +0700

    add middleware

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.use(compression());
+  app.use(helmet());
 
   // Setup Swagger
   const config = new DocumentBuilder()
